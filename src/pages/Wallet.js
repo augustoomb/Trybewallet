8fe97e8c7c4db99fc9151f71d139463f4f9009fc
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-05-19 08:55:39 -0300

    correcao req3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -14,53 +14,58 @@ class Wallet extends React.Component {
     const pagamento = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
     const tag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
 
-    if (!isLoading && currencies) {
-      return (
-        <div>
-          <header>
-            <span data-testid="email-field">{ email }</span>
-            <span data-testid="total-field">0</span>
-            <span data-testid="header-currency-field">BRL</span>
-          </header>
-          <form>
-            <input type="number" name="valor" data-testid="value-input" />
-            <input type="text" name="descricao" data-testid="description-input" />
-            <label htmlFor="moeda">
-              Moeda
-              <select name="moeda" id="moeda">
-                {
-                  currencies.map((currency, index) => (
-                    <option key={ index }>{ currency }</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label htmlFor="metPag">
-              Método de pagamento
-              <select name="metPag" data-testid="method-input">
-                {
-                  pagamento.map((pag, index) => (
-                    <option key={ index }>{ pag }</option>
-                  ))
-                }
-              </select>
-            </label>
-            <label htmlFor="tag">
-              Tag
-              <select name="tag" data-testid="tag-input">
-                {
-                  tag.map((pag, index) => (
-                    <option key={ index }>{ pag }</option>
-                  ))
-                }
-              </select>
-            </label>
-          </form>
-        </div>
-      );
-    }
-    if (isLoading) { return <div>Loading...</div>; }
-    return <div>Não foi possível</div>;
+    // if (!isLoading && currencies) {
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">{ email }</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        {
+          !isLoading && currencies
+            ? (
+              <form>
+                <input type="number" name="valor" data-testid="value-input" />
+                <input type="text" name="descricao" data-testid="description-input" />
+                <label htmlFor="moeda">
+                  Moeda
+                  <select name="moeda" id="moeda">
+                    {
+                      currencies.map((currency, index) => (
+                        <option key={ index }>{ currency }</option>
+                      ))
+                    }
+                  </select>
+                </label>
+                <label htmlFor="metPag">
+                  Método de pagamento
+                  <select name="metPag" data-testid="method-input">
+                    {
+                      pagamento.map((pag, index) => (
+                        <option key={ index }>{ pag }</option>
+                      ))
+                    }
+                  </select>
+                </label>
+                <label htmlFor="tag">
+                  Tag
+                  <select name="tag" data-testid="tag-input">
+                    {
+                      tag.map((pag, index) => (
+                        <option key={ index }>{ pag }</option>
+                      ))
+                    }
+                  </select>
+                </label>
+              </form>)
+            : <div>Loading...</div>
+        }
+      </div>
+    );
+    // }
+    // if (isLoading) { return <div>Loading...</div>; }
+    // return <div>Não foi possível</div>;
   }
 }
 
